2fa9187ec28a83cd23883f8f8a6202568497def3
Sahil Dhanju <sahildhanju@improbable.io>
2018-08-05 21:46:59 +0100

    Added schema codegeneration

--- Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/SpatialGDKEditorInteropCodeGenerator.cpp
+++ Plugins/SpatialGDK/SpatialGDKEditorToolbar/Source/SpatialGDKEditorToolbar/Private/InteropCodeGenerator/SpatialGDKEditorInteropCodeGenerator.cpp
@@ -261,7 +261,7 @@ bool SpatialGDKGenerateInteropCode()
 	FString AbsoluteCombinedIntermediatePath = FPaths::ConvertRelativePathToFull(CombinedIntermediatePath);
 	FPlatformFileManager::Get().GetPlatformFile().CreateDirectoryTree(*AbsoluteCombinedIntermediatePath);
 
-	const FString CombinedForwardingCodePath = FPaths::Combine(*FPaths::GetPath(FPaths::GameSourceDir()), *GetOutputPath(ConfigFilePath));
+	const FString CombinedForwardingCodePath = FPaths::Combine(*FPaths::GetPath(FPaths::GameSourceDir()), *GetOutputPath(ConfigFilePath).Append(TEXT("Typebindings/")));
 	FString AbsoluteCombinedForwardingCodePath = FPaths::ConvertRelativePathToFull(CombinedForwardingCodePath);
 
 	UE_LOG(LogSpatialGDKInteropCodeGenerator, Display, TEXT("Schema path %s - Forwarding code path %s"), *AbsoluteCombinedSchemaPath, *AbsoluteCombinedForwardingCodePath);
@@ -300,12 +300,229 @@ bool SpatialGDKGenerateInteropCode()
 	}
 
 	// Run Codegen
-	const FString CodegenPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("Scripts/Codegen.bat")));
-	if (!RunProcess(CodegenPath, TEXT("")))
+	//const FString CodegenPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("Scripts/Codegen.bat")));
+	//if (!RunProcess(CodegenPath, TEXT("")))
+	//{
+	//	UE_LOG(LogSpatialGDKInteropCodeGenerator, Error, TEXT("Spatial C++ Worker Codegen failed. Path: '%s'."), *CodegenPath);
+	//	return false;
+	//}
+
+	const FString SchemaCompilerPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("Binaries/ThirdParty/Improbable/Programs/schema_compiler.exe")));
+
+	const FString GeneratedSchemaPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("../spatial/schema/improbable/unreal/generated/")));
+	const FString CoreSchemaPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("../spatial/schema/improbable/unreal/gdk/")));
+	const FString StandardSchemaPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("Binaries/ThirdParty/Improbable/Programs/schema/improbable")));
+
+	const FString CppOutputPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GameSourceDir()), *GetOutputPath(ConfigFilePath).Append(TEXT("Cpp/"))));
+	const FString JsonOutputPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("Intermediate/Improbable/Json")));
+
+	TArray<FString> GeneratedSchemaFiles;
+	IFileManager::Get().FindFiles(GeneratedSchemaFiles, *GeneratedSchemaPath, nullptr);
+	TArray<FString> CoreSchemaFiles;
+	IFileManager::Get().FindFiles(CoreSchemaFiles, *CoreSchemaPath, nullptr);
+	TArray<FString> StandardSchemaFiles;
+	IFileManager::Get().FindFiles(StandardSchemaFiles, *StandardSchemaPath, nullptr);
+
+	FString AllSchema = TEXT("");
+
+	for(const FString& SchemaFile : GeneratedSchemaFiles)
 	{
-		UE_LOG(LogSpatialGDKInteropCodeGenerator, Error, TEXT("Spatial C++ Worker Codegen failed. Path: '%s'."), *CodegenPath);
-		return false;
+		FString FullSchemaPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*GeneratedSchemaPath, *SchemaFile));
+		AllSchema.Append(*FullSchemaPath).Append(TEXT(" "));
 	}
+
+	for(const FString& SchemaFile : CoreSchemaFiles)
+	{
+		FString FullSchemaPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*CoreSchemaPath, *SchemaFile));
+		AllSchema.Append(*FullSchemaPath).Append(TEXT(" "));
+	}
+
+	for(const FString& SchemaFile : StandardSchemaFiles)
+	{
+		FString FullSchemaPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*StandardSchemaPath, *SchemaFile));
+		AllSchema.Append(*FullSchemaPath).Append(TEXT(" "));
+	}
+
+	const FString GeneratedSchemaCompilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("../spatial/schema/")));
+	const FString StandardSchemaCompilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::GetPath(FPaths::GetProjectFilePath()), TEXT("Binaries/ThirdParty/Improbable/Programs/schema/")));
+
+	const FString CodegenArguments = FString::Printf(TEXT("--cpp_out=%s --ast_json_out=%s --schema_path=%s --schema_path=%s --load_all_schema_on_schema_path %s"), *CppOutputPath, *JsonOutputPath, *GeneratedSchemaCompilePath, *StandardSchemaCompilePath, *AllSchema);
+
+	FPlatformFileManager::Get().GetPlatformFile().CreateDirectory(*CppOutputPath);
+	FPlatformFileManager::Get().GetPlatformFile().CreateDirectory(*JsonOutputPath);
+
+	RunProcess(SchemaCompilerPath, CodegenArguments);
+
+
+	FString GeneratedSchemaJsonPath = JsonOutputPath;
+	GeneratedSchemaJsonPath.Append(TEXT("/improbable/unreal/generated"));
+	TArray<FString> GeneratedSchemaJsonFiles;
+	IFileManager::Get().FindFiles(GeneratedSchemaJsonFiles, *GeneratedSchemaJsonPath, nullptr);
+
+	FString CoreSchemaJsonPath = JsonOutputPath;
+	CoreSchemaJsonPath.Append(TEXT("/improbable/unreal/gdk"));
+	TArray<FString> CoreSchemaJsonFiles;
+	IFileManager::Get().FindFiles(CoreSchemaJsonFiles, *CoreSchemaJsonPath, nullptr);
+
+	FString StandardSchemaJsonPath = JsonOutputPath;
+	StandardSchemaJsonPath.Append(TEXT("/improbable"));
+	TArray<FString> StandardSchemaJsonFiles;
+	IFileManager::Get().FindFiles(StandardSchemaJsonFiles, *StandardSchemaJsonPath, nullptr);
+
+	TArray<FString> AllSchemaComponents;
+
+	for(const FString& SchemaJson : GeneratedSchemaJsonFiles)
+	{
+		FString FullJsonPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*GeneratedSchemaJsonPath, *SchemaJson));
+		FString SchemaJson;
+
+		FFileHelper::LoadFileToString(SchemaJson, *FullJsonPath);
+
+		TSharedPtr<FJsonObject> JsonParsed;
+		TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(SchemaJson);
+		if (FJsonSerializer::Deserialize(JsonReader, JsonParsed))
+		{
+			TArray<TSharedPtr<FJsonValue>> ComponentArray = JsonParsed->GetArrayField("componentDefinitions");
+
+			for(const TSharedPtr<FJsonValue>& Element : ComponentArray)
+			{
+				FString componentName = Element->AsObject()->GetStringField("qualifiedName");
+				componentName = componentName.Replace(TEXT("."), TEXT("::"));
+
+				const TCHAR* test = *componentName;
+				auto a = 1;
+
+				AllSchemaComponents.Add(componentName);
+			}
+		}
+	}
+
+	for(const FString& SchemaJson : StandardSchemaJsonFiles)
+	{
+		FString FullJsonPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*StandardSchemaJsonPath, *SchemaJson));
+		FString SchemaJson;
+
+		FFileHelper::LoadFileToString(SchemaJson, *FullJsonPath);
+
+		TSharedPtr<FJsonObject> JsonParsed;
+		TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(SchemaJson);
+		if (FJsonSerializer::Deserialize(JsonReader, JsonParsed))
+		{
+			TArray<TSharedPtr<FJsonValue>> ComponentArray = JsonParsed->GetArrayField("componentDefinitions");
+
+			for(const TSharedPtr<FJsonValue>& Element : ComponentArray)
+			{
+				FString componentName = Element->AsObject()->GetStringField("qualifiedName");
+				componentName = componentName.Replace(TEXT("."), TEXT("::"));
+
+				const TCHAR* test = *componentName;
+				auto a = 1;
+
+				AllSchemaComponents.Add(componentName);
+			}
+		}
+	}
+
+	for (const FString& SchemaJson : CoreSchemaJsonFiles)
+	{
+		FString FullJsonPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*CoreSchemaJsonPath, *SchemaJson));
+		FString SchemaJson;
+
+		FFileHelper::LoadFileToString(SchemaJson, *FullJsonPath);
+
+		TSharedPtr<FJsonObject> JsonParsed;
+		TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(SchemaJson);
+		if (FJsonSerializer::Deserialize(JsonReader, JsonParsed))
+		{
+			TArray<TSharedPtr<FJsonValue>> ComponentArray = JsonParsed->GetArrayField("componentDefinitions");
+
+			for (const TSharedPtr<FJsonValue>& Element : ComponentArray)
+			{
+				FString componentName = Element->AsObject()->GetStringField("qualifiedName");
+				componentName = componentName.Replace(TEXT("."), TEXT("::"));
+
+				const TCHAR* test = *componentName;
+				auto a = 1;
+
+				AllSchemaComponents.Add(componentName);
+			}
+		}
+	}
+
+	FCodeWriter Writer;
+
+	Writer.Printf(R"""(
+		// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+		// Note that this file has been generated automatically
+		#pragma once
+
+		#include <improbable/worker.h>)""");
+	Writer.PrintNewLine();
+
+	TArray<FString> ComponentHeaders;
+
+	for(const FString& Component : GeneratedSchemaFiles)
+	{
+		FString ComponentHeader = Component.Replace(TEXT(".schema"), TEXT(".h"));
+		ComponentHeader.InsertAt(0, TEXT("improbable/unreal/generated"));
+
+		ComponentHeaders.Add(ComponentHeader);
+	}
+
+	for(const FString& Component : CoreSchemaFiles)
+	{
+		FString ComponentHeader = Component.Replace(TEXT(".schema"), TEXT(".h"));
+		ComponentHeader.InsertAt(0, TEXT("improbable/unreal/gdk"));
+
+		ComponentHeaders.Add(ComponentHeader);
+	}
+
+	for(const FString& Component : StandardSchemaFiles)
+	{
+		FString ComponentHeader = Component.Replace(TEXT(".schema"), TEXT(".h"));
+		ComponentHeader.InsertAt(0, TEXT("improbable/"));
+
+		ComponentHeaders.Add(ComponentHeader);
+	}
+
+	for(const FString& ComponentHeader : ComponentHeaders)
+	{
+		Writer.Printf("#include \"%s\"", *ComponentHeader);
+	}
+
+	Writer.PrintNewLine();
+
+	Writer.Print(R"""(
+namespace improbable
+{
+namespace unreal
+{
+	using Components = worker::Components<)""");
+
+	Writer.Indent();
+	Writer.Indent();
+	for(int i = 0; i < AllSchemaComponents.Num(); i++)
+	{
+		const FString& Component = AllSchemaComponents[i];
+
+		if(i == AllSchemaComponents.Num() - 1)
+		{
+			Writer.Printf("%s>", *Component);
+			break;
+		}
+
+		Writer.Printf("%s,", *Component);
+	}
+
+	Writer.PrintNewLine();
+	Writer.Outdent();
+	Writer.Outdent();
+	Writer.Print("} // ::unreal");
+	Writer.Print("} // ::improbable");
+
+	Writer.WriteToFile(TEXT("woo.abc"));
+
+	Writer.WriteToFile(FString::Printf(TEXT("%s%s.h"), *CombinedForwardingCodePath, TEXT("SpatialComponents")));
 
 	return true;
 }
